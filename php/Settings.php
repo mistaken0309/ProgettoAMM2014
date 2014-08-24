8bd812a968e4df876c56624fe34d0ab08cb9e3f2
Annalisa <Annalisa@HP-21-6-2011>
2014-08-24 16:13:43 +0200

    Modifiche a Setting.php

--- php/Settings.php
+++ php/Settings.php
@@ -30,7 +30,7 @@ class Settings {
                     break;
                 case 'spano.sc.unica.it':
                     // configurazione pubblica
-                    self::$appPath = 'http://' . $_SERVER['HTTP_HOST'] . '/amm2014/congiuAnnalisa/php/';
+                    self::$appPath = 'http://' . $_SERVER['HTTP_HOST'] . '/amm2014/congiuAnnalisa/';
                     break;
                 
                 default:
