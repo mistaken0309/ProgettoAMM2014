2e14930efb9808b1c1137af3eb0c187df87e444d
Annalisa Congiu <>
2014-09-06 23:12:10 +0200

    commit 12 - controllo funzionamento generale dell'applicazione (correzione degli errori)

--- php/view/venditore/aggiungi_manga.php
+++ php/view/venditore/aggiungi_manga.php
@@ -4,16 +4,16 @@
 <div class="input-anagrafica">
 
     <form method="post" action="venditore/aggiungi-manga">
-        <label class="label_manga" for="titolo">Titolo</label>
+        <label class="label_manga" class="label_manga" for="titolo">Titolo</label>
         <input type="text" name="titolo" id="titolo" value=""/>
         <br/>
-        <label for="titolo_originale">Titolo Originale</label>
+        <label class="label_manga" for="titolo_originale">Titolo Originale</label>
         <input type="text" name="titolo_originale" id="titolo_originale" value=""/>
         <br/>
-        <label for="n_volume">Numero Volume</label>
+        <label class="label_manga" for="n_volume">Numero Volume</label>
         <input type="text" name="n_volume" id="n_volume" value=""/>
         <br/>
-        <label for="autore">Autore</label>
+        <label class="label_manga" for="autore">Autore</label>
         <select name="autore" id="autore">
             
             <?php
@@ -26,16 +26,16 @@
             
         </select>
         <br/>
-        <label for="casa_ed">CasaEditrice</label>
+        <label class="label_manga" for="casa_ed">CasaEditrice</label>
         <input type="text" name="casa_ed" id="casa_ed" value=""/>
         <br/>
-        <label for="pubblicazione">AnnoPubblicazione</label>
+        <label class="label_manga" for="pubblicazione">AnnoPubblicazione</label>
         <input type="text" name="pubblicazione" id="pubblicazione" value=""/>
         <br/>
-        <label for="lingua">Lingua</label>
+        <label class="label_manga" for="lingua">Lingua</label>
         <input type="text" name="lingua" id="lingua" value=""/>
         <br/>
-        <label for="categoria">Categoria</label>
+        <label class="label_manga" for="categoria">Categoria</label>
         
         <select name="categoria" id="categoria">
             
@@ -49,16 +49,16 @@
             
         </select>
         <br/>
-        <label for="genere">Genere</label>
+        <label class="label_manga" for="genere">Genere</label>
         <input type="text" name="genere" id="genere" value=""/>
         <br/>
-        <label for="descrizione">Descrizione</label>
+        <label class="label_manga" for="descrizione">Descrizione</label>
         <input type="text" name="descrizione" id="descrizione" value=""/>
         <br/>
-        <label for="prezzo">Prezzo</label>
+        <label class="label_manga" for="prezzo">Prezzo</label>
         <input type="text" name="prezzo" id="prezzo" value=""/>
         <br/>
-        <label for="n_articoli">NumeroArticoli</label>
+        <label class="label_manga" for="n_articoli">NumeroArticoli</label>
         <input type="text" name="n_articoli" id="n_articoli" value=""/>
         <br/>
         <input type="hidden" name="cmd" value="aggiungi">
--- php/view/venditore/manga.php
+++ php/view/venditore/manga.php
@@ -2,7 +2,7 @@
 <h3 class="manga-title"><?= $manga->getTitolo() ?></h3>
 <div class="manga"><h4>Titolo Originale</h4>: <?= $manga->getTitoloOriginale() ?></div>
 <div class="manga"><h4># volume</h4>: <?= $manga->getNumeroVolume() ?></div>
-<div class="manga"><h4>Autore</h4>: <a href="venditore/lista_per_autore?param=<?= $manga->getAutore()?>"><?= AutoreFactory::instance()->getAutorePerId($manga->getAutore())->getAutore() ?></a></div>
+<div class="manga"><h4>Autore</h4>: <?= AutoreFactory::instance()->getAutorePerId($manga->getAutore())->getAutore() ?></div>
 <div class="manga"><h4>Casa editrice</h4>: <?= $manga->getCasaEditrice() ?></div>
 <div class="manga"><h4>Anno di pubblicazione</h4>: <?= $manga->getAnnoPubblicazione() ?></div>
 <div class="manga"><h4>Lingua</h4>: <?= $manga->getLingua() ?></div>
