e515b60300d9fcea5864e98bba1edb63dd9a36e0
Annalisa Congiu <>
2014-09-06 23:19:16 +0200

    commit 12 - controllo funzionamento generale dell'applicazione (correzione degli errori)

--- php/view/home/leftbar-home.php
+++ php/view/home/leftbar-home.php
@@ -2,6 +2,5 @@
     <h2>Navigazione</h2>
     <div>       
         <p class="page"><a href="home">Home</a></p>
-        <p class="page"><a href="home/lista">Sfoglia i manga</a></p>
     </div>
 </nav>
--- php/view/login/leftbar-login.php
+++ php/view/login/leftbar-login.php
@@ -1,6 +1,6 @@
 <nav>
     <h2>Navigazione</h2>
     <div>       
-        <p class="page"><a href="home/lista">Sfoglia i manga</a></p>
+        <p class="page"><a href="home">Home</a></p>
     </div>
 </nav>
