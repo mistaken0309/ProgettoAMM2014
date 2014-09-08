c6445f3e13576de5034160a11063f7b2e83a74b9
Annalisa <Annalisa@HP-21-6-2011>
2014-09-08 19:59:12 +0200

    commit 13 - aggiunta pagina di descrizione

--- php/view/home/home-content.php
+++ php/view/home/home-content.php
@@ -1,8 +1,5 @@
 <?php
 switch ($vista->getSottoPagina()) {
-    case 'lista':
-        include_once 'lista.php';
-        break;
     case 'lista_per_autore':
         include_once 'lista_per_autore.php';
         break;
@@ -12,35 +9,10 @@ switch ($vista->getSottoPagina()) {
     case 'login':
         include_once 'content-login.php';
         break;
+    case 'lista':
     default:
-        
-?>
-<h2>Elenco manga</h2>
-
-
-<table>
-    <thead>
-        <tr>
-            <th>Titolo</th>
-            <th>Autore</th>
-            <th>Prezzo</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php foreach ($mangas as $manga) { ?>
-        <tr>
-            <?php
-            $mangaid = $manga->getId();
-            ?>
-            <td><a href="home/manga?param=<?= "$mangaid" ?>"><?= $manga->getTitolo() ?></a></td>
-            <td><a href="home/lista_per_autore?param=<?= $manga->getAutore()?>"><?= AutoreFactory::getAutorePerId($manga->getAutore())->getAutore() ?></a></td>
-            <td><?= $manga->getPrezzo() ?></td>
-        </tr>
-        <?php } ?>
-    </tbody>
-</table>
-<?php
-    break;
+        include_once 'lista.php';
+        break;
 }
 ?>
 
