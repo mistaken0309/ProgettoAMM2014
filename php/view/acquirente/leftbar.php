334c673938505cf85cc11d36d9a78d48425853a6
Annalisa Congiu <>
2014-09-16 09:54:08 +0200

    commit 16 - correzione errori

--- php/view/acquirente/leftbar.php
+++ php/view/acquirente/leftbar.php
@@ -1,7 +1,7 @@
 <nav>
     <h2>Navigazione</h2>
     <div>       
-        <!--<p class="page"><a href="acquirente/home">Home</a></p>-->
+        <p class="page"><a href="acquirente/home">Home</a></p>
         <p class="page"><a href="acquirente/anagrafica">Impostazioni Account</a></p>
         <p class="page"><a href="acquirente/lista">Sfoglia i Manga</a></p>
         <p class="page"><a href="acquirente/acquisti">Storico Acquisti</a></p>
